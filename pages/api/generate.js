5a14f07df4d6ebedd6a37db6ef003c39b6663494
celestemarieb <34022835+celestemarieb@users.noreply.github.com>
2023-01-31 21:56:37 +1100

    updated basepromptprefix

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,14 +5,14 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = "";
+const basePromptPrefix = `Write me a friendly, polite and conversational answer in the style of a middle-aged professional American`;
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `${basePromptPrefix}${req.body.userInput}`\n,
     temperature: 0.7,
     max_tokens: 250,
   });
